405568c500e4ad493b969befad93405ca5560048
Benjamin Ejlertsen <benjaminejlertsen@hotmail.com>
2016-12-05 18:30:49 +0100

    Added performance tracker with saved screenshots

--- Main.py
+++ Main.py
@@ -2,27 +2,35 @@ import NeuralNetwork
 import numpy as np
 import gym
 import time
+from colorama import Fore
+import PerformanceView
+import pygame
+
+# pygame.init()
+# pygame.display.set_caption("Stats")
+# bgCol = (255,255,255)
+# frontCol = (0,0,0)
+# screen = pygame.display.set_mode((600,200))
+# frameRate = 60
+# clock = pygame.time.Clock()
 
 env = gym.make('CartPole-v0')
 env.reset()
 print(env.action_space)
 print(env.observation_space)
 
-#Take action
-#Get reward
-#Get new observations
-#Update Weights using reward and observertions and choose action
-
 action = [0]
 rewardDelta  = np.zeros((2))
 
 action[0] = env.action_space.sample()
 
-nn = NeuralNetwork.NeuralNetwork(1,9,3,12,1)
+inputs = 5
 
-state = np.zeros((1,9))
+state = np.zeros((1,inputs))
 
 env.reset()
+
+performance = PerformanceView.PerformanceView((1200,600))
 
 a = 0
 
@@ -35,86 +43,161 @@ averageReward = 0
 learningRate = 2
 render = False
 
-for episode in range(100000):
-    obs = env.reset()
-    #oldObs = obs
+avgRewardLast100Episodes = []
 
-    rewardSum = 0
-    if episode % 100 == 0:
-        print("##### Average reward: " +  str(averageReward/100) + " #####")
-        if averageReward/100 >= 195:
-            print("###################### WON WON WON ########################")
-            print("################# After " + str(episode-100) + " episodes ######################")
-        averageReward = 0
-
-    for step in range(200):
-        env.render()
-
-        #oldOldObs = oldObs
-        oldObs = obs
-        obs, reward, done, info = env.step((a)) # take a random action
-
-        state[0,0:4] = obs
-        state[0,5:9] = oldObs
-        #state[0,9:13] = oldOldObs
-
-        if done: reward = -1
-
-        averageReward += reward
-
-        rewardSum += reward
-
-        for i in range(2):
-            state[0,4] = i
-            nn.setData(state,reward)
-            rewardDelta[i] = nn.train(1,learningRate)
-
-        a = round(float(np.argmax(rewardDelta)))
-
-        if done or step >= 199:
-
-            if (rewardSum > 195): learningRate = 0
-
-            if rewardSum >= 195:
-                rewardStreak += 1
-                if rewardStreak > bestRewardStreak:
-                    bestRewardStreak = rewardStreak
-            else:
-                rewardStreak = 0
-                render = False
-
-            if rewardStreak > 99:
-                render = True
+hiddenLayers = [2]
+neurons = [12,13,14,15,16]
+alpha = [2,2.5,3,3.5,4,4.5]
+alphaDecrease = [0.7,0.9,0.95,0.99,0.999,1]
 
 
+hiddenLayers = [3,4]
+neurons = [12,14,16]
+alpha = [4,5]
+alphaDecrease = [0.85,0.9,0.95,.99,.999,0.999]
+minAlpha = 0.0000001
 
-            if rewardSum > bestReward:
-                bestReward = rewardSum
+np.random.seed(1)
 
-            if(episode % 10 == 0): print("End of episode " + str(episode) + "\n reward: " + str(rewardSum) + "\n Highest reward: " + str(bestReward) + "\n Learning rate: " + str(learningRate)
-                                         + "\n Reward streak: " + str(rewardStreak) + "\n Best reward streak: " + str(bestRewardStreak))
-            break;
+maxEpisodes = 2000
+
+saves = 0
+filenum = 0
+
+attempts = 2
+settingID = 0
+
+for h in hiddenLayers:
+
+    for n in neurons:
+
+        for alp in alpha:
+
+            for aD in alphaDecrease:
+
+                for attempt in range(attempts+1):
+
+                    nn = NeuralNetwork.NeuralNetwork(1, inputs, h, n, 1)
+
+                    learningRate = alp
+                    learningRateDecrease = aD
+                    print("\n")
+                    print(Fore.LIGHTBLUE_EX)
+                    print("######################### SETTINGS ###########################")
+                    print("Hidden layers: ", h)
+                    print("Neurons: ", n)
+                    print("Alpha: ", alp)
+                    print("Alpha Decrease: ", aD)
+                    print(Fore.RESET)
+
+                    for i in range(len(avgRewardLast100Episodes)):
+                        avgRewardLast100Episodes[i] = 0
+
+                    averageReward = 0
+
+                    settings = [h, n, alp, aD]
+
+                    print("ATTEMPT STATS: ", attempt, attempts)
+                    if attempt == 0:
+                        saves += 1
+                        settingID += 1
+
+                    if attempt > 0:
+                        if saves > 4:
+                            saves = 0
+                            filenum += 1
+                            performance.saveScreenshot("data_"+str(filenum))
+                            del performance
+                            performance = PerformanceView.PerformanceView((1200, 600))
+
+                        performance.addPerformance(settingID, episodesToWin,settings)
+
+                    performance.update(maxEpisodes,attempts)
+
+                    for episode in range(maxEpisodes+1):
+                        obs = env.reset()
+
+                        if episode % 500 == 0:
+                            print("Episode: " + str(episode))
+                            print("Avg Reward: ", averageReward)
+
+                        rewardSum = 0
+
+                        if episode > 0:
+                            for i in range(len(avgRewardLast100Episodes)):
+                                averageReward += avgRewardLast100Episodes[i]
+                            averageReward /= len(avgRewardLast100Episodes)
+
+                            if episode % 500 == 0:
+                                print("\nEpisode: ", episode)
+                                print("  ##### Average reward last 100 ep.: " +  str(averageReward) + " #####")
+                                print("ALPHA: ", learningRate)
+
+                            if averageReward >= 195:
+                                print(Fore.LIGHTCYAN_EX + "")
+                                print("###################### WON WON WON ########################")
+                                print("################# After " + str(episode-100) + " episodes ######################")
+                                print("##### Average reward last 100 ep.: " + str(averageReward) + " #####")
+                                print(avgRewardLast100Episodes)
+                                print(Fore.RESET + "")
+                                episodesToWin = episode-100
+                                averageReward = 0
+                                break
+
+                        for step in range(200):
+                            #env.render()
+
+                            #oldOldObs = oldObs
+                            oldObs = obs
+                            obs, reward, done, info = env.step((a)) # take a random action
+
+                            state[0,0:4] = obs
+                            #state[0,5:9] = oldObs
+                            #state[0,9:13] = oldOldObs
+
+                            if done: reward = -1
+
+                            #averageReward += reward
+
+                            rewardSum += reward
+
+                            for i in range(2):
+                                state[0,4] = i
+                                nn.setData(state,reward)
+                                rewardDelta[i] = nn.train(1,learningRate)
+
+                            a = round(float(np.argmax(rewardDelta)))
+
+                            if done or step >= 199:
+
+                                episodesToWin = episode
+
+                                if len(avgRewardLast100Episodes) == 100:
+                                    avgRewardLast100Episodes.remove(avgRewardLast100Episodes[0])
+
+                                avgRewardLast100Episodes.append(rewardSum)
 
 
-        #nn.setData(state,reward)
+                                if (rewardSum > 195):
+                                    learningRate *= learningRateDecrease
+                                    learningRate = max(learningRate,minAlpha)
 
+                                if rewardSum >= 195:
+                                    rewardStreak += 1
+                                    if rewardStreak > bestRewardStreak:
+                                        bestRewardStreak = rewardStreak
+                                else:
+                                    rewardStreak = 0
+                                    render = False
 
+                                if rewardStreak > 99:
+                                    render = True
 
+                                if rewardSum > bestReward:
+                                    bestReward = rewardSum
 
+                                #if(episode % 100 == 0): print("End of episode " + str(episode) + "\n reward: " + str(rewardSum) + "\n Highest reward: " + str(bestReward) + "\n Learning rate: " + str(learningRate)
+                                #                             + "\n Reward streak: " + str(rewardStreak) + "\n Best reward streak: " + str(bestRewardStreak))
+                                break;
 
-#
-# x = np.array([[0,0,1],
-# [0,1,1],
-# [1,0,1],
-# [1,1,1]])
-#
-# y = np.array([[0],
-# [1],
-# [1],
-# [0]])
-#
-# nn = NeuralNetwork.NeuralNetwork(x.shape[0],x.shape[1],1,4,1)
-#
-# nn.setData(x,y)
-#
-# nn.train(60000)
+                        #clock.tick(frameRate)
---
+++ PerformanceView.py
@@ -0,0 +1,95 @@
+import pygame
+import numpy as np
+import math
+
+
+class PerformanceView():
+
+    def __init__(self,screenSize, distFromEdge = 50, caption = "Performance", bgCol = (255,255,255), frontCol = (10,50,140)):
+
+        pygame.init()
+        self.__graphCol = ([[220,20,20],[220,220,20],[20,220,20],[20,20,220],[20,220,220],[220,20,220],[20,20,20]])
+                            #[150, 20, 20], [150, 150, 20], [20, 150, 20], [20, 20, 150], [20, 150, 150], [150, 20, 150],
+                            #[70, 70, 70]])
+        pygame.display.set_caption(caption)
+        self.__distFromEdge = distFromEdge
+        self.__settingID = -1
+        self.__performances = []
+        self.__performanceSettings = []
+        self.__performancesSaved = -1
+        self.__bgCol = bgCol
+        self.__frontCol = frontCol
+        self.__screenSize = screenSize
+        self.__screen = pygame.display.set_mode(screenSize)
+        self.__frameRate = 30
+        self.__clock = pygame.time.Clock()
+
+        self.__font = pygame.font.Font(None,25)
+
+        #self.__performances = np.zeros((attemptsPrSetting,maxEpisodes,numberOfSettings))
+
+        # for i in range(maxEpisodes):
+        #     self.__performances.append([])
+        #     for j in range(numberOfSettings):
+        #         self.__performances[i].append(0)
+
+    def update(self,maxEpisodes,maxFreq):
+
+        graphW = self.__screenSize[0]-self.__distFromEdge*2
+        graphH = self.__screenSize[1]-self.__distFromEdge*5
+        barWidth = 3
+        binSize = 100
+        totalBins = int(maxEpisodes/binSize)+1
+        binData = []
+
+
+        self.__screen.fill(self.__bgCol)
+
+        legendName = self.__font.render("Hidden Layers, Neurons Pr. Layer, Alpha, Alpha Decrease", True, [0,0,0])
+        self.__screen.blit(legendName, [self.__distFromEdge, self.__distFromEdge-25])
+
+        for i in range(self.__performancesSaved+1):
+            binData.append([0] * totalBins)
+
+            for x in range(len(self.__performanceSettings[0])):
+                legendText = self.__font.render(str(self.__performanceSettings[i][x]), True,self.__graphCol[i])
+                self.__screen.blit(legendText, [self.__distFromEdge+125*x, self.__distFromEdge + 25 * i])
+
+            for j in range(len(self.__performances[i])):
+                binData[i][math.floor(self.__performances[i][j]/binSize)] += 1
+
+        print("BIN DATA ##### ::: ",binData)
+
+        for attempt in range(self.__performancesSaved+1):
+            for bin in range(totalBins):
+
+                if attempt == 0:
+                    text = self.__font.render(str(binSize * bin), True, self.__graphCol[self.__performancesSaved])
+                    self.__screen.blit(text, [self.__distFromEdge + bin * (graphW / totalBins),
+                                              self.__screenSize[1] - self.__distFromEdge + 25])
+
+                if binData[attempt][bin] > 0:
+                    pygame.draw.rect(self.__screen, self.__graphCol[attempt], [self.__distFromEdge+bin*(graphW/totalBins)+(barWidth*attempt),
+                                self.__screenSize[1] - self.__distFromEdge, barWidth, -binData[attempt][bin]/maxFreq*graphH])
+                    text = self.__font.render(str(binData[attempt][bin]), True, self.__graphCol[attempt])
+                    self.__screen.blit(text, [self.__distFromEdge+bin*(graphW/totalBins)+(barWidth*attempt),
+                                              ((self.__screenSize[1] - self.__distFromEdge) - binData[attempt][bin] / maxFreq * graphH) - 25])
+
+            # pygame.draw.rect(self.__screen, self.__frontCol,
+            #                  [self.__distFromEdge + barWidth * j, self.__screenSize[1] - self.__distFromEdge, 10,
+            #                   -self.__performances[i][j] / maxEpisodes * graphH])
+
+        pygame.display.flip()
+
+    def addPerformance(self, settingID, episodesToWin, settings):
+
+        if self.__settingID != settingID:
+            self.__settingID = settingID
+            self.__performancesSaved += 1
+            self.__performances.append([])
+            self.__performanceSettings.append(settings)
+
+        self.__performances[self.__performancesSaved].append(episodesToWin)
+
+    def saveScreenshot(self, filename):
+        pygame.image.save(self.__screen, filename+".png")
